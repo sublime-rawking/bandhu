4e13440e9176549c1b886d9194067348db7e9bae
tejasSublime <tejas.sublimetechnocorp@gmail.com>
2024-07-01 18:01:11 +0530

    added give and ask calendar view

---
+++ src/app/api/admin/auth/route.js
@@ -0,0 +1,143 @@
+import prisma from "@/config/dbConfig";
+import { GenerateToken } from "@/config/jwtConfig";
+import { decrypt, encrypt } from "@/helper/security";
+import validator from "@/helper/validate";
+import { Promise } from "es6-promise";
+import moment from "moment/moment";
+
+
+/**
+ * Handles the POST request to create a new user in the system.
+ *
+ * @param {Request} request - The request object containing the user data.
+ * @return {Promise<Response>} A promise that resolves to the response object.
+ * The response object contains the success status, a message, and the created user data.
+ * If there is an error, the response object contains the error message.
+ */
+export async function POST(request) {
+    try {
+        const bodyData = await request.json();
+        console.log(bodyData);
+        const { error, status } = await new Promise((resolve) => {
+            validator(bodyData, {
+                username: "required",
+                email: "required",
+                password: "required",
+            }, {}, (error, status) => {
+                resolve({ error, status });
+            });
+        });
+
+        if (!status) {
+            return Response.json({
+                success: false,
+                message: "validation error",
+                data: {},
+                error
+            }, { status: 400 });
+        }
+
+        const encryptedPassword = encrypt(bodyData.password);
+        const user = await prisma.users.create({
+            data: {
+                username: bodyData.username,
+                password: encryptedPassword,
+                email: bodyData.email,
+                last_login: moment().toDate(),
+                status: 1
+            },
+            select: {
+                id: true,
+                username: true,
+                email: true,
+                last_login: true
+            }
+        });
+
+
+        const token = GenerateToken({ id: user.id });
+        user.token = token;
+
+
+        return Response.json({
+            success: true,
+            message: "User created successfully",
+            data: user
+        }, { status: 200 });
+    } catch (error) {
+        console.error(error);
+
+        return Response.json({ success: false, message: error.message, data: {}, error }, { status: 500 });
+    }
+}
+
+
+/**
+ * Handles the GET request to authenticate a user.
+ *
+ * @param {Request} request - The request object containing user credentials.
+ * @return {Response} JSON response with authentication status and user data.
+ */
+export async function GET(request) {
+    try {
+
+        const searchParams = new URL(request.url).searchParams;
+
+
+        const { error, status } = await new Promise((resolve) => {
+            validator({ ...Object.fromEntries(searchParams) }, {
+                username: "required",
+                password: "required",
+            }, {}, (error, status) => {
+                resolve({ error, status });
+            });
+        });
+
+        if (!status) {
+            return Response.json({
+                success: false,
+                message: "validation error",
+                data: {},
+                error
+            }, { status: 400 });
+        }
+
+        const user = await prisma.users.findFirst({
+            where: {
+                username: searchParams.get("username"),
+                status: 1
+            },
+        });
+
+
+        if (!user) {
+            return Response.json({
+                success: false,
+                message: "User not found",
+                data: {}
+            }, { status: 404 });
+        }
+
+
+        if (decrypt(user.password) !== searchParams.get("password")) {
+            return Response.json({
+                success: false,
+                message: "Invalid credentials",
+                data: {}
+            }, { status: 401 });
+        }
+
+        const token = GenerateToken({ id: user.id });
+        user.token = token;
+        delete user.password;
+        return Response.json({
+            success: true,
+            message: "User logged in successfully",
+            data: user
+        }, { status: 200 });
+    } catch (error) {
+        console.error(error);
+
+        return Response.json({ success: false, message: error.message, data: {}, error }, { status: 500 });
+    }
+}
---
+++ src/app/api/mobile/give_and_ask/calendar/route.js
@@ -0,0 +1,75 @@
+import prisma from "@/config/dbConfig";
+import { VerifyToken } from "@/config/jwtConfig";
+import moment from "moment/moment";
+
+
+export const GET = async (request) => {
+    try {
+
+        const token = await VerifyToken();
+        if (!token.success) {
+            return Response.json(token, { status: 401 });
+        }
+
+
+        const searchParams = new URL(request.url).searchParams;
+        const id = searchParams.get('id');
+        let giveAndAsk = []
+        if (searchParams.get("date")) {
+            giveAndAsk = await prisma.give_ask.findMany({
+                where: {
+                    member_id: id ? Number(id) : token.data.id,
+                    date: {
+                        gte: moment(searchParams.get("date"), "YYYY-MM").startOf('month').toDate(),
+                        lte: moment(searchParams.get("date"), "YYYY-MM").endOf('month').toDate(),
+                    }
+                },
+                orderBy: {
+                    date: 'asc'
+                }
+            });
+
+
+        } else {
+
+
+            giveAndAsk = await prisma.give_ask.findMany({
+                where: {
+                    member_id: id ? Number(id) : token.data.id
+                },
+                orderBy: {
+                    date: 'asc'
+                }
+
+            });
+        }
+
+
+        const groupedData = giveAndAsk.reduce((acc, item) => {
+            const date = item.date.toISOString().split('T')[0];
+            if (!acc[date]) {
+                acc[date] = { length: 0 };
+            }
+            acc[date].length++;
+            acc[date].date = date;
+            return acc;
+        }, {});
+
+        const groupedDataArray = Object.values(groupedData);
+        return Response.json({
+            success: true,
+            message: "give and ask successfully",
+            data: groupedDataArray,
+        }, { status: 200 });
+
+
+    } catch (error) {
+        console.error(error);
+        return Response.json({
+            success: false,
+            message: error.message,
+            error: error,
+            data: {},
+        })
+    }
+} 
